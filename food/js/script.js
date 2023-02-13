8e176b6db1a5a810b01bd13dfbe7704b5159c301
prycepavel <pryce.pavel@gmail.com>
2023-02-13 15:43:22 +0300

    include the axios library

--- food/js/script.js
+++ food/js/script.js
@@ -235,38 +235,19 @@ window.addEventListener('DOMContentLoaded', () => {
     return await res.json();
   };
 
-  // Первый вариант реализации
-  getResource('http://localhost:3000/menu')
+  // getResource('http://localhost:3000/menu')
+  //   .then(data => {
+  //     data.forEach(({img, altimg, title, descr, price}) => {
+  //       new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+  //     });
+  //   });
+
+  axios.get('http://localhost:3000/menu')
     .then(data => {
-      data.forEach(({img, altimg, title, descr, price}) => {
+      data.data.forEach(({img, altimg, title, descr, price}) => {
         new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
       });
     });
-
-  // Второй вариант реализации для вывода один раз
-  // getResource('http://localhost:3000/menu')
-  //   .then(data => createCard(data));
-  
-  // function createCard(data) {
-  //   data.forEach(({ img, altimg, title, descr, price }) => {
-  //     let element = document.createElement('div');
-  //     price = price * 70;
-  //     element.classList.add('menu__item');
-
-  //     element.innerHTML = `
-  //     <img src=${img} alt=${altimg}>
-  //     <h3 class="menu__item-subtitle">${title}</h3>
-  //     <div class="menu__item-descr">${descr}</div>
-  //     <div class="menu__item-divider"></div>
-  //     <div class="menu__item-price">
-  //       <div class="menu__item-cost">Цена:</div>
-  //       <div class="menu__item-total"><span>${price}</span> руб/день</div>
-  //     </div>
-  //     `;
-
-  //     document.querySelector('.menu .container').append(element);
-  //   });
-  // }
 
   //  Forms
 
